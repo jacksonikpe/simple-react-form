5cf26ea7f25f33cd30f5263d07d9a7a33dc537c2
Jackson Collins-Ikpe <jacksoncikpe@gmail.com>
2022-09-03 13:56:37 +0100

    styled form

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>hello</p>
+    <div className='form-container'>
+      <form className='register-form'>
+        <input className='form-field' type="text" placeholder='First Name' />        
+        <input className='form-field' type="text" placeholder='Last Name' />        
+        <input className='form-field' type="email" placeholder='Email' />  
+        <button className='form-field' type='submit'>SUBMIT</button>      
+      </form>      
     </div>
   );
 }
