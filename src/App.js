a8bb8248e5f3de07cdf2581982b0f505b115f192
Jackson Collins-Ikpe <jacksoncikpe@gmail.com>
2022-09-03 18:06:34 +0100

    added functionality to form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState({
+    firstName:'',
+    secondName:'',
+    email:''
+  })
+  const [submit, setSubmit] = useState(false)
+  const [validate, setValidate] = useState(false)
+
+  //function that stores input change to the state
+  const onInputChange = (e) => {
+    let data = {
+      [e.target.id] : e.target.value
+    }
+    setValue({...value, ...data});
+  }
+
+  //function to submit form
+  const submitForm = (e) => {
+    e.preventDefault();
+    setSubmit(true);
+    if(value.firstName && value.secondName && value.email) {
+      setValidate(true)
+    }
+  }
+
   return (
     <div className='form-container'>
-      <form className='register-form'>
-        <input className='form-field' type="text" placeholder='First Name' />        
-        <input className='form-field' type="text" placeholder='Last Name' />        
-        <input className='form-field' type="email" placeholder='Email' />  
+      <form className='register-form' onSubmit={submitForm}>
+        {submit && validate ? <div className="success-message">Thank you for registering!</div> : null}
+        <input className='form-field' type="text" onChange={onInputChange} id='firstName' value={value.firstName} placeholder='First Name' />  
+        {submit && !value.firstName ? <span>Type in your first name</span> : null}      
+        <input className='form-field' type="text" onChange={onInputChange} id='secondName' value={value.secondName} placeholder='Last Name' />    
+        {submit && !value.secondName ? <span>Type in your second name</span> : null}    
+        <input className='form-field' type="email" onChange={onInputChange} id='email' value={value.email} placeholder='Email' />  
+        {submit && !value.email ? <span>Type in your email</span> : null}
         <button className='form-field' type='submit'>SUBMIT</button>      
       </form>      
     </div>
